32faca81df52d5400c7c1899222d2fffee110ba6
codeTony22 <jgonz275@fiu.edu>
2018-08-22 00:12:23 -0400

    JWT Implemented for user login.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://jgonz:dinamita12@ds125862.mlab.com:25862/devsocialconnector"
+    mongoURI: "mongodb://jgonz:dinamita12@ds125862.mlab.com:25862/devsocialconnector",
+    secretOrKey: "secret"
 }
