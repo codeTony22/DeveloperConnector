1cc89578b60ca02f58f683449f04213c5c1e5bd8
codeTony22 <jgonz275@fiu.edu>
2018-08-24 15:19:54 -0400

    Added validation to the register and login routes.

--- validation/register.js
+++ validation/register.js
@@ -1,16 +1,52 @@
-const validator = require('validator');
+const Validator = require('validator');
 const isEmpty   = require('./is-empty');
 
 module.exports = function validateRegisterInput( data ) {
     let errors = {};
 
-    if( !validator.isLength(data.name, {min: 2, max: 30})){
+    // Check if data.name is a string, if false set as an empty string
+    data.name = !isEmpty( data.name ) ? data.name : '';
+    data.email = !isEmpty( data.email ) ? data.email : '';
+    data.password = !isEmpty( data.password ) ? data.password : '';
+    data.password2 = !isEmpty( data.password2 ) ? data.password2 : '';
+
+
+    // Check if the name is in the min and max range
+    if( !Validator.isLength(data.name, {min: 2, max: 30})){
         errors.name = 'Name must be between 2 and 30 characters';
+    }
+    // Check if the name field is empty
+    if(Validator.isEmpty( data.name )) {
+        errors.name = 'Name field is required';
+    }
+    // Check if the email field is empty
+    if(Validator.isEmpty( data.email )) {
+        errors.email = 'Email field is required';
+    }
+    // Check if the email is a valid email
+    if(!Validator.isEmail( data.email )) {
+        errors.email = 'Email is invalid';
+    }
+    // Check if the password field is empty
+    if(Validator.isEmpty( data.password )) {
+        errors.password = 'Password field is required';
+    }
+    // Validate the password field
+    if(!Validator.isLength( data.password, {min: 6 , max: 30 } )) {
+        errors.password = 'Password must be at least 6 characters';
+    }
+    // Check if the password2 field is empty
+    if(Validator.isEmpty( data.password2 )) {
+        errors.password = 'Confirm Password field is required';
+    }
+    // Check if password and password2 match
+    if(!Validator.equals(data.password, data.password2)) {
+        errors.password2 = 'Passwords must match';
     }
 
 
     return {
         errors,
         isValid: isEmpty(errors)
-    }
+    };
 };
